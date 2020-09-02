f1ed395c1789be57342746160c0429340a8d9044
Gabriel Anguiano <anguianog97@gmail.com>
2020-09-02 14:28:40 -0700

    mvp day 1. player can navigate through rooms

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -39,6 +39,7 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +50,42 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+def play_game():
+    current_room = room['outside']
+    player_name = input('Enter your name to play:')
+    player = Player(current_room, player_name)
+    command = ""
+    while command != 'q':
+
+        command = input(f"{player.name}!! Your current location is {current_room.name}."
+                        f" {current_room.description}.To navigate, insert the commands: n,e,s,w. To quit: insert q.")
+        if command == 'n':
+            if current_room.n_to is None:
+                print("you cant go there")
+            current_room = current_room.n_to
+            print(f"Your are in {current_room.name}, {current_room.description}")
+
+        if command == 'w':
+            if current_room.w_to is None:
+                print("You cant go there")
+            else:
+                current_room = current_room.w_to
+
+        if command == 's':
+            if current_room.s_to is None:
+                print("you can't go there")
+            else:
+                current_room = current_room.s_to
+        if command == 'e':
+            if current_room.e_to is None:
+                print("You cant go there")
+            else:
+                current_room = current_room.e_to
+
+
+    if command == 'q':
+        print('Thank you for playing!! :D')
+
+
+play_game()
